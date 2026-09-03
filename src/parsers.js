f13042cd669ea0d2856e26e82f1be78da3326ed5
VadimGug <vadimethod@gmail.com>
2026-09-03 15:20:40 +0300

    replace if to switch case in parsers

--- src/parsers.js
+++ src/parsers.js
@@ -1,14 +1,15 @@
 import yaml from 'js-yaml';
 
 const parse = (data, format) => {
-  if (format === 'json') {
-    return JSON.parse(data);
+  switch (format) {
+    case 'json':
+      return JSON.parse(data);
+    case 'yml':
+    case 'yaml':
+      return yaml.load(data);
+    default:
+      throw new Error(`Unknown format: '${format}'`);
   }
-  if (format === 'yml' || format === 'yaml') {
-    return yaml.load(data);
-  }
-
-  throw new Error(`Unknown format: '${format}'`);
 };
 
 export default parse;
